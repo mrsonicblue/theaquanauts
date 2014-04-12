af67d3a950144f7c7ed351aa54f6072a7c128faf
Eric <mr.sonicblue@gmail.com>
2014-04-12 17:55:24 -0500

    Star party calendar

---
+++ wp-content/plugins/star-party-calendar/star-party-calendar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Star Party Calendar
+ * Plugin URI: http://example.com
+ * Description: To The Moon!
+ * Version: 1.0
+ * Author: Benny
+ * Author URI: http://example.com
+ * License: None
+ */
+
+function spc_starparty( $atts ){
+	return 'aklsdjaskldjalksdj lkasdjkl asjd';
+}
+add_shortcode('starparty', 'spc_starparty');
+
+?>
