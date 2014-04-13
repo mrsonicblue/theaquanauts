60bfd3a9a90e691d389d8f04ace2d07a55eff30d
Eric <mr.sonicblue@gmail.com>
2014-04-13 03:13:52 -0500

    Full page template

---
+++ wp-content/themes/flat-theme/page-fullwidth.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+            <?php /* The loop */ ?>
+            <?php while ( have_posts() ) { the_post(); ?>
+                <?php edit_post_link( __( 'Edit', ZEETEXTDOMAIN ), '<small class="edit-link pull-right ">', '</small><div class="clearfix"></div>' ); ?>
+                <?php if ( has_post_thumbnail() && ! post_password_required() ) { ?>
+                <div class="entry-thumbnail">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+                <?php } ?>
+                    <?php the_content(); ?>
+                    <?php zee_link_pages(); ?>
+            <?php// comments_page(); ?>
+            <?php } ?>
+<?php get_footer();
