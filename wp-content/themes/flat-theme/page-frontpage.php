75bde2e3bd40d1c386f9346a0c9bbc4734eff521
Creepmobile <email@steef.us>
2014-04-21 00:48:23 -0500

    Added a news header above the news feed.

--- wp-content/themes/flat-theme/page-frontpage.php
+++ wp-content/themes/flat-theme/page-frontpage.php
@@ -222,6 +222,7 @@ $total_sliders = count($sliders);
 							'post-type' => 'post',
 							'category' => 'uncategorized'
 						));?>
+						<h1>News</h1>
             <?php while ( $query->have_posts() ) { $query->the_post();  ?>
 							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 									<div class="entry-content">
